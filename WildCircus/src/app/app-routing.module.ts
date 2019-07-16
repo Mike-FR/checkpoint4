0f7040f92542702c97e2d849819ff61917a4ca8b
Mickael Spennato <mickael.spennato@gmail.com>
2019-07-17 00:23:46 +0200

    ajustement CSS

--- WildCircus/src/app/app-routing.module.ts
+++ WildCircus/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { ArtistsComponent } from './artists/artists.component';
 import { ReservationComponent } from './reservation/reservation.component';
+import { AdminpageComponent } from './adminpage/adminpage.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomepageComponent, data: { animation: 'homePage' } },
   { path: 'artists', component: ArtistsComponent, data: { animation: 'artistPage' } },
   { path: 'reservation', component: ReservationComponent, data: { animation: 'reservationPage' } },
+  { path: 'admin', component: AdminpageComponent }
 ];
 
 @NgModule({
